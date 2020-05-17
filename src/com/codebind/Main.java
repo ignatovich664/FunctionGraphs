7ba048b3c0cc9fb53d2a3bce46ee53a919a736c7
ignatovich <ignatovich>
2020-05-17 11:22:24 +0300

    Добавлена главная панель для вывода основного интерфейса программы

--- src/com/codebind/Main.java
+++ src/com/codebind/Main.java
@@ -1,19 +1,25 @@
 package com.codebind;
 
+import com.codebind.UI.MainPanel;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.Locale;
 
 public class Main {
+    public static MainPanel mainPanel;
     /**
      * Функция создающая интерфейс приложения
      */
+
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         JFrame frame = new JFrame();
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(new Dimension(1000, 800));
+        mainPanel = new MainPanel();
+        frame.add(mainPanel);
         frame.setVisible(true);
     }
 }
---
+++ src/com/codebind/UI/MainPanel.java
@@ -0,0 +1,6 @@
+package com.codebind.UI;
+
+import javax.swing.*;
+
+public class MainPanel extends JPanel {
+}
